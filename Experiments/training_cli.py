8a2587fea9cf32289bbb7cfb715a4daa92820614
Andrey Shor <andreyshor1999@gmail.com>
2024-04-25 22:18:38 -0500

    uploaded corrected version of the test file for peft experiments

--- Experiments/training_cli.py
+++ Experiments/training_cli.py
@@ -1,5 +1,15 @@
 '''
-Command Line Tool to allow one to run this on a GPU-As-A-Service Provider such as Lambda Labs or Hyperstack
+Command Line Tool to allow one to run this on a GPU-As-A-Service Provider such as Lambda Labs or Hyperstack for a single GPU
 '''
 
 import argparse
+
+
+def peft_experiments():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    peft_experiments()
