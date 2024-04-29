9ae17bb4b0f806a75a24c749325f6c18753751af
Micaela V <micaela.v.mccall@gmail.com>
2024-04-28 19:54:10 -0600

    reorg

--- Experiments/eval_utils.py
+++ Experiments/eval_utils.py
@@ -1,4 +1,5 @@
 import os
+import time
 import json
 import torch
 import evaluate
@@ -10,9 +11,96 @@ rouge = evaluate.load("rouge")
 cosine_similarity = evaluate.load("bertscore")
 perplexity = evaluate.load("perplexity", module_type="metric")
 
+
+
+# Prompt ICL functions
+def preprocess_prompt_icl(hf_model: str, loaded_tokenizer:AutoTokenizer, ds: Dataset, experiment, k_shot: int=1,
+               max_k_shot_token_length=200, seed=42, sample: int=1000):
+    ds = ds.shuffle(seed=seed)
+    eval_sample = ds.select(range(sample))
+
+    loaded_tokenizer = AutoTokenizer.from_pretrained(hf_model, device_map={"": 0})
+
+    def filter_by_token_length(example):
+        tokens = loaded_tokenizer(example['text'], return_tensors="pt", truncation=False)
+        return tokens.input_ids.size(1) <= max_k_shot_token_length
+
+
+
+    print(f'Running prompt injection for: {experiment}')
+    prompt_insert = "Answer this question truthfully:"
+
+    if experiment == 'zero_shot':
+        prompt_insert = "Answer the question truthfully:"
+        results = process_samples(eval_sample, hf_model, prompt_insert, loaded_tokenizer)
+
+    elif experiment == 'k_shot':
+        filtered_dataset_for_k_shot =  ds.filter(filter_by_token_length)
+        print(f"Number of examples in the dataset: {len(filtered_dataset_for_k_shot)}")
+        if len(filtered_dataset_for_k_shot) < k_shot:
+            raise ValueError(f"Dataset has less than {k_shot} examples")
+
+        prompt_insert = "Answer the question truthfully. Follow these examples:"
+        prompt_insert += "\n".join(filtered_dataset_for_k_shot['questions'][:k_shot])
+        prompt_insert += "\n"
+        prompt_insert += 'Question:'
+
+        results = process_samples(eval_sample, hf_model, prompt_insert, loaded_tokenizer)
+    eval_sample = datasets.concatenate_datasets([eval_sample, results], axis=1)
+
+    return eval_sample
+
+def process_samples(sample_data, model_name, prompt_insert, tokenizer):
+    model_to_insert_point = {
+        'google/gemma-7b': "user",
+        'meta-llama/Llama-2-7b-hf': "<s>",
+        'mistralai/Mistral-7B-v0.1': "[INST]"
+    }
+    
+    original_dataset = []
+    new_tokenizations = []
+
+    for example in sample_data:
+        text = example['questions']
+        insertion_point = text.find(model_to_insert_point[model_name]) + len(model_to_insert_point[model_name])
+        new_text = text[:insertion_point] + " " + prompt_insert + " " + text[insertion_point:]
+        
+        inputs = tokenizer(new_text, return_tensors="pt")  
+        original_dataset.append(example['id'].split('-')[0])
+        new_tokenizations.append(inputs.input_ids)
+    processed_samples = {'prompt_tokenizations': new_tokenizations, 'original_dataset': original_dataset}
+    out = Dataset.from_dict(processed_samples)
+    print(out['prompt_tokenizations'])
+    return out
+
+
 # Prediction functions
+
+def predict(trained_model:SFTTrainer, tokenizer:AutoTokenizer, eval_sample:Dataset, model_name:str, prompted:bool=False):
+    if prompted==True:
+        assert 'prompt_tokenizations' in list(eval_sample.features.keys()), f"Eval Data needs the following column: 'prompt_tokenizations', but instead has { list(eval_sample.features.keys()) }"
+        token_col = 'prompt_tokenizations'
+    else:
+        assert 'input_ids' in list(eval_sample.features.keys()), f"Eval Data needs the following column: 'input_ids', but instead has { list(eval_sample.features.keys()) }"
+        token_col = 'input_ids'
+    eval_sample = eval_sample.with_format("torch")
+    predictions = []
+    latencies = []
+    for inp in eval_sample[token_col]:
+        # inp = torch.tensor(inp, dtype=int)
+        start = time.time()
+        outp = trained_model.generate(inp, max_new_tokens=20, return_dict_in_generate=True, output_scores=True)
+        end = time.time()
+        pred = tokenizer.batch_decode(outp['sequences'], skip_special_tokens=True)
+
+        predictions.append(pred[0])
+        latencies.append(end - start)
+
+    return predictions, latencies
+
+
 def strip_output_text(output:str, model_name:str):
-  processed = output 
+  processed = output
   if model_name == 'google/gemma-7b':
     if 'Answer:' in processed:
       processed = processed[processed.find("Answer:")+7:]
@@ -47,7 +135,7 @@ def prediction_wrapper(trained_model:SFTTrainer, tokenizer:AutoTokenizer, ds:Dat
             original_dataset.append(example['id'].split('-')[0])
         eval_sample = datasets.concatenate_datasets([ds, Dataset.from_dict({'original_dataset': original_dataset})], axis=1)
         return eval_sample
-    
+
     if add_prompt == True and sample > 0:
          eval_sample = preprocess_prompt_icl(model_name, tokenizer, ds, experiment='zero_shot', sample=sample, seed=seed)
     elif add_prompt == False and sample > 0:
@@ -56,12 +144,11 @@ def prediction_wrapper(trained_model:SFTTrainer, tokenizer:AutoTokenizer, ds:Dat
         eval_sample = add_dataset_name_col(sample_data)
     elif add_prompt == True and sample == 0:
          eval_sample = preprocess_prompt_icl(model_name, tokenizer, ds, experiment='zero_shot', sample=ds.shape[0], seed=seed)
-    else: 
+    else:
         eval_sample = add_dataset_name_col(ds)
-    
+    print("eval_sample generated")
     predictions, latencies = predict(trained_model, tokenizer, eval_sample, model_name, prompted=add_prompt)
-    print(predictions)
-    print(eval_sample['questions'])
+    print("predictions generated")
     # predictions = [predictions[i][len(eval_sample['questions'][i]):] for i in range(len(eval_sample['questions']))]
     predictions = [strip_output_text(s, model_name) for s in predictions]
 
@@ -69,7 +156,7 @@ def prediction_wrapper(trained_model:SFTTrainer, tokenizer:AutoTokenizer, ds:Dat
 
 
     pred_ds = Dataset.from_dict({
-        'predictions': [p.lower() for p in predictions], 
+        'predictions': [p.lower() for p in predictions],
         'ground_truth':answers_stripped,
         'original_dataset':eval_sample['original_dataset'],
         'latencies': latencies})
@@ -86,42 +173,19 @@ def prediction_wrapper(trained_model:SFTTrainer, tokenizer:AutoTokenizer, ds:Dat
     return pred_ds
 
 
-def predict(trained_model:SFTTrainer, tokenizer:AutoTokenizer, eval_sample:Dataset, model_name:str, prompted:bool=False):
-    if prompted==True:
-        assert 'prompt_tokenizations' in list(eval_sample.features.keys()), f"Eval Data needs the following column: 'prompt_tokenizations', but instead has { list(eval_sample.features.keys()) }"
-        token_col = 'prompt_tokenizations'
-    else:
-        assert 'input_ids' in list(eval_sample.features.keys()), f"Eval Data needs the following column: 'input_ids', but instead has { list(eval_sample.features.keys()) }"
-        token_col = 'input_ids'
-
-    predictions = []
-    latencies = []
-    for inp in eval_sample[token_col]:
-        inp = torch.tensor(inp, dtype=int)
-        start = time.time()
-        outp = trained_model.generate(inp, max_new_tokens=20, return_dict_in_generate=True, output_scores=True)
-        end = time.time()
-        pred = tokenizer.batch_decode(outp['sequences'], skip_special_tokens=True)
-
-        predictions.append(pred[0])
-        latencies.append(end - start)
-
-    return predictions, latencies
-
 # Metrics
 def compute_accuracy(scores:list):
     num_correct = 0
     for score in scores:
         if score == 1:
             num_correct += 1
-    
+
     accuracy = 100.0 * ((num_correct) / len(scores))
     return accuracy
 
 
-# This is currently wrong; needs a string for model_id that points to a TRAINED model (which we don't have yet)
 def compute_ppl(predictions, trained_model):
-
+    # This is currently wrong; needs a string for model_id that points to a TRAINED model (which we don't have yet)
     ppl = perplexity.compute(predictions=predictions, model_id=trained_model)['mean_perplexity']
     return ppl
 
@@ -145,7 +209,6 @@ def compute_similarity(predictions:list, ground_truth:list):
     print('computing cosine similarity')
     scores = cosine_similarity.compute(predictions=predictions, references=ground_truth, model_type="distilbert-base-uncased")
     return scores['f1']
-
 
 
 def compute_scores(original_dataset:str, predictions:list, ground_truth:list):
@@ -209,7 +272,7 @@ def load_saved_predictions(file_path:str):
 
 def predict_and_evaluate(trained_model:SFTTrainer, tokenizer:AutoTokenizer, ds:Dataset, model_name:str, add_prompt:bool=False, sample:int=1000, seed:int=42, return_predictions:bool=False):
       print("calculating predictions")
-      pred_ds = prediction_wrapper(trained_model, tokenizer, ds, add_prompt, sample, seed)
+      pred_ds = prediction_wrapper(trained_model, tokenizer, ds, model_name, add_prompt, sample, seed)
       print("calculating metrics")
       metrics = evaluate_predictions(pred_ds, model_name)
 
@@ -218,63 +281,3 @@ def predict_and_evaluate(trained_model:SFTTrainer, tokenizer:AutoTokenizer, ds:D
       else:
         return metrics
 
-
-# Prompt ICL functions
-def preprocess_prompt_icl(hf_model: str, loaded_tokenizer:AutoTokenizer, ds: Dataset, experiment, k_shot: int=1,
-               max_k_shot_token_length=200, seed=42, sample: int=1000):
-    ds = ds.shuffle(seed=seed)
-    eval_sample = ds.select(range(sample))
-
-    loaded_tokenizer = AutoTokenizer.from_pretrained(hf_model, device_map={"": 0})
-
-    def filter_by_token_length(example):
-        tokens = loaded_tokenizer(example['text'], return_tensors="pt", truncation=False)
-        return tokens.input_ids.size(1) <= max_k_shot_token_length
-
-
-
-    print(f'Running prompt injection for: {experiment}')
-    prompt_insert = "Answer this question truthfully:"
-
-    if experiment == 'zero_shot':
-        prompt_insert = "Answer the question truthfully:"
-        results = process_samples(eval_sample, hf_model, prompt_insert, loaded_tokenizer)
-
-    elif experiment == 'k_shot':
-        filtered_dataset_for_k_shot =  ds.filter(filter_by_token_length)
-        print(f"Number of examples in the dataset: {len(filtered_dataset_for_k_shot)}")
-        if len(filtered_dataset_for_k_shot) < k_shot:
-            raise ValueError(f"Dataset has less than {k_shot} examples")
-
-        prompt_insert = "Answer the question truthfully. Follow these examples:"
-        prompt_insert += "\n".join(filtered_dataset_for_k_shot['questions'][:k_shot])
-        prompt_insert += "\n"
-        prompt_insert += 'Question:'
-
-        results = process_samples(eval_sample, hf_model, prompt_insert, loaded_tokenizer)
-    eval_sample = datasets.concatenate_datasets([eval_sample, results], axis=1)
-
-    return eval_sample
-
-def process_samples(sample_data, model_name, prompt_insert, tokenizer):
-    model_to_insert_point = {
-        'google/gemma-7b': "user",
-        'meta-llama/Llama-2-7b-hf': "<s>",
-        'mistralai/Mistral-7B-v0.1': "[INST]"
-    }
-    
-    original_dataset = []
-    new_tokenizations = []
-
-    for example in sample_data:
-        text = example['questions']
-        insertion_point = text.find(model_to_insert_point[model_name]) + len(model_to_insert_point[model_name])
-        new_text = text[:insertion_point] + " " + prompt_insert + " " + text[insertion_point:]
-        
-        inputs = tokenizer(new_text, return_tensors="pt")  
-        original_dataset.append(example['id'].split('-')[0])
-        new_tokenizations.append(inputs.input_ids)
-    processed_samples = {'prompt_tokenizations': new_tokenizations, 'original_dataset': original_dataset}
-    out = Dataset.from_dict(processed_samples)
-    print(out['prompt_tokenizations'])
-    return out
